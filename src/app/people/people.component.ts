175d1cde856aad45adc9ec23c87233e2451ce3ac
Aldo Sanchez <rodrigosanchezg8@gmail.com>
2020-11-18 15:55:39 -0600

    People frequency count button for modal open

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -4,6 +4,8 @@ import {Observable, Subscription} from 'rxjs';
 import {APIResponse} from '../interfaces/api-response';
 import {Person} from '../models/person';
 import {TableState} from '../interfaces/table-state';
+import {NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
+import {FrequencyCountModalComponent} from './frequency-count-modal/frequency-count-modal.component';
 
 @Component({
   selector: 'app-people',
@@ -17,20 +19,32 @@ export class PeopleComponent implements OnInit, OnDestroy {
   private people: Person[];
 
   public tableState: TableState = {
-    columns: ['Name', 'Email address', 'Job title', 'Actions'],
+    columns: ['Name', 'Email address', 'Job title'],
     loading: true
   };
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private modalService: NgbModal) {
   }
 
   ngOnInit(): void {
+    this.fetchPeople();
+  }
+
+  fetchPeople(): void {
     this.peopleObservable = this.http.get('v2/people');
     this.peopleSubscribable = this.peopleObservable.subscribe((obs: APIResponse) => {
       this.tableState.loading = true;
       this.people = Person.deserializeMany(obs.data);
       this.tableState.loading = false;
     });
+  }
+
+  openFrequencyCountModal(): void {
+    const modalRef: NgbModalRef = this.modalService.open(FrequencyCountModalComponent);
+    if (this.people && this.people.length) {
+      modalRef.componentInstance.people = this.people;
+    }
   }
 
   ngOnDestroy(): void {
