9d4d5323b1c84bc722c43fb401825596efa5c69c
Aldo Sanchez <rodrigosanchezg8@gmail.com>
2020-12-16 01:19:50 -0600

    AddPersonModal component open and validator

--- src/app/modules/people/people.module.ts
+++ src/app/modules/people/people.module.ts
@@ -6,6 +6,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FrequencyCountModalComponent} from './frequency-count-modal/frequency-count-modal.component';
 import {DuplicatedPeopleModalComponent} from './duplicated-people-modal/duplicated-people-modal.component';
 import {ToastModule} from '../../shared/components/toast/toast.module';
+import {AddPersonModalComponent} from './add-person-modal/add-person-modal.component';
+import {FormsModule} from '@angular/forms';
 
 const routes: Routes = [
   {path: '', component: PeopleComponent}
@@ -16,9 +18,11 @@ const routes: Routes = [
     PeopleComponent,
     FrequencyCountModalComponent,
     DuplicatedPeopleModalComponent,
+    AddPersonModalComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
     NgbModule,
     ToastModule
